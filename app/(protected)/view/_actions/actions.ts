594e38db8e344b13421a2fa1f3cc136536ace156
Nitesh Kakkar <121416598+Nitesh-04@users.noreply.github.com>
2025-01-26 00:12:52 +0530

    fix: order rounds by time desc

--- app/(protected)/view/_actions/actions.ts
+++ app/(protected)/view/_actions/actions.ts
@@ -53,7 +53,7 @@ export async function fetchRoundByApplicationId(applicationId: string) {
   try {
     const rounds = await prisma.round.findMany({
       where: { applicationId },
-      orderBy: { roundDateTime: "asc" },
+      orderBy: { roundDateTime: "desc" },
     });
 
     const currentTime = new Date();
